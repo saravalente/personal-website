783fa711e2f0d2c35158383b3a33e4eeb0b79ec4
saravalente <saravalente@college.harvard.edu>
2019-01-07 21:52:04 -0600

    Image resizing is now responsive.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,11 @@ $( document ).ready(function() {
   	$("#about2").hide();
   	$("#work").hide();
   	$("#projects").hide();
+  	$("#stick").hide();
     $("#title").fadeIn(1000, function() {
+      $("#stick").delay(200).fadeIn(1000);
 	  $("#about").fadeIn(1000, function() {
 	  	$("#about2").fadeIn(1000, function() {
-	  		$("#work").fadeIn(1000);
 	  		$("#projects").fadeIn(1500);
 	  	});
 	  });
